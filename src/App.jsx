cbea386baec79041f4ae4ed02371a31e8c58c367
shilpavakkayil <shilpavpurushothaman@gmail.com>
2024-10-30 12:22:41 +0000

    basic layout of the home page

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,12 @@ function App() {
   return (
       <div className='main-container'>
         <h1>Gen AI App</h1>
+        <div className='form-container'>
+          <div>
+            <label>Enter Query</label>
+            <input type="text" className= "query-input" placeholder='Place holder'/>
+          </div>
+        </div>
       </div>
       
   )
